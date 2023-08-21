3b85aac6fdcecf281c5eadae5eb42461df587881
João Nuno <joaomnuno@gmail.com>
2023-08-21 15:30:11 +0100

    somthing is not right idk why :(

--- frontend/src/grid.js
+++ frontend/src/grid.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Grid({ letters }) {
   return (
-    <div class="grid gap-0">
+    <div class="h-2 grid gap-0">
       {letters.map((row, rowIndex) => (
         <div key={rowIndex} class="flex">
           {row.map((letter, colIndex) => (
             <div
               key={colIndex}
-              class="flex-1 aspect-w-1 aspect-h-1 border p-2 text-center"
+              class="flex-1 aspect-w-1 aspect-h-1 border-2 text-center"
             >
-              <div class="aspect-square flex justify-center items-center">
+              <div class="duration-300 aspect-square flex justify-center items-center hover:bg-slate-300 hover:cursor-pointer select-none">
                 {letter}
               </div>
             </div>
