e297770f475811ec017f2767a8ee1a20b483a50c
João Nuno <joaomnuno@gmail.com>
2023-08-23 00:26:59 +0100

    improvement!

--- frontend/src/grid.js
+++ frontend/src/grid.js
@@ -2,17 +2,12 @@ import React from "react";
 
 function Grid({ letters }) {
   return (
-    <div class="h-2 grid gap-0">
+    <div class="h-2 flex gap-0">
       {letters.map((row, rowIndex) => (
-        <div key={rowIndex} class="flex">
+        <div key={rowIndex} class="">
           {row.map((letter, colIndex) => (
-            <div
-              key={colIndex}
-              class="flex-1 aspect-w-1 aspect-h-1 border-2 text-center"
-            >
-              <div class="duration-300 aspect-square flex justify-center items-center hover:bg-slate-300 hover:cursor-pointer select-none">
-                {letter}
-              </div>
+            <div class="h-10 w-10 aspect-w-1 aspect-h-1 flex hover:bg-slate-300 justify-center items-center border hover:cursor-pointer select-none">
+              {letter}
             </div>
           ))}
         </div>
