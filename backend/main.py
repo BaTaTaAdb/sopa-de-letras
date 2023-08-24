c2b0325d0472d7e01e915b0abb88d641e22ca23f
João Nuno <joaomnuno@gmail.com>
2023-08-24 20:24:08 +0100

    added get-words api endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,43 @@
+from typing import Any
+
 from fastapi import FastAPI
 from gamesdl import game_logic
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost:3000",
+    "http://10.7.154.235",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
+
+LETTERS = [
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+]
 
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/get-words', response_model=list[list[str]])
+async def get_letters() -> Any:
+    return LETTERS
