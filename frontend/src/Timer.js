ec2400e9a89157f145c3bfd07f6f31420ac785a0
BaTaTaAdb <joaominecraft.adr@gmail.com>
2024-02-07 11:06:13 +0000

    timer initialized stopped

--- frontend/src/Timer.js
+++ frontend/src/Timer.js
@@ -3,24 +3,32 @@ import WordContext from './WordContext';
 
 const Timer = ({ timeInSeconds }) => {
     const [time, setTime] = useState(timeInSeconds);
-    const { endGame } = useContext(WordContext);
+    const { endGame, timerRunning, stopTimer } = useContext(WordContext);
     let interval;
 
     useEffect(() => {
-        // Interval should be set inside the effect
-        const interval = setInterval(() => {
-            setTime((prevTime) => {
-                if (prevTime <= 1) {
-                    clearInterval(interval); // Clear interval when time is up
-                    endGame(); // End the game when time reaches 0
-                    return 0; // Set time to 0
-                }
-                return prevTime - 1; // Decrement time
-            });
-        }, 1000);
-        // Cleanup function to clear interval
+        let interval;
+
+        if (timerRunning) {
+            interval = setInterval(() => {
+                setTime(prevTime => {
+                    if (prevTime <= 1) {
+                        clearInterval(interval);
+                        endGame();
+                        stopTimer(false); // Stop the timer
+                        return 0;
+                    }
+                    return prevTime - 1;
+                });
+            }, 1000);
+        } else if (!timerRunning && time !== timeInSeconds) {
+            // Optional: Reset the timer when stopped
+            setTime(timeInSeconds);
+        }
+
         return () => clearInterval(interval);
-    }, [endGame]); // Empty dependency array means this runs once on mount
+    }, [time, timerRunning, timeInSeconds, endGame, stopTimer]);
+
 
 
     useEffect(() => {
