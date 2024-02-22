917a6b2654d6526a3d73a6145eecc43a27015da9
Pedro Elias <pedro.acse@gmail.com>
2024-02-22 12:08:08 +0000

    changed to english

--- backend/filter.py
+++ backend/filter.py
@@ -1,7 +1,7 @@
-with open("backend/words.txt", "r") as file:
+with open("backend/nouns.txt", "r") as file:
     words = file.readlines()
     # print(words[0:50])
-    corrected_words = [word for word in words if len(word.split()) == 1]
+    corrected_words = [word for word in words if len(word.split()) == 1 and len(word) < 10 and len(word) > 3]
 
 with open("backend/corrected_words.txt", "w") as file:
     file.writelines(corrected_words)
